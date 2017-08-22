cb3b275c4cbe51e945b1b0ff5626132f4e11d7c7
Maxime Daniel <root@maxux.net>
2017-08-22 12:08:24 +0200

    Improve ubuntu bootstrapping script

--- flist_builder/ubuntu_flist_builder.sh
+++ flist_builder/ubuntu_flist_builder.sh
@@ -2,20 +2,28 @@
 set -e
 
 # This script require privileged access
-if (whoami != root);then
+if (whoami != root); then
     echo "Please run the script using sudo"
-else
-    apt-get update
-    apt-get install -y debootstrap
-    mkdir -p /mnt/ubuntu
-    debootstrap --include git-core,curl,ca-certificates,tmux,openssh-client,openssh-server,vim --arch amd64 xenial /mnt/ubuntu http://archive.ubuntu.com/ubuntu/
-    sed -i "s/main/main restricted universe multiverse/" /mnt/ubuntu/etc/apt/sources.list
-    chroot /mnt/ubuntu /bin/bash -c "apt-get update && apt-get install redis-server -y"
-    curl -s https://raw.githubusercontent.com/Jumpscale/developer/master/mascot?$RANDOM > /mnt/ubuntu/root/.mascot.txt
-    touch /mnt/ubuntu/root/.iscontainer
-    chmod 1777 /mnt/ubuntu/tmp
-    rm -rf /mnt/ubuntu/etc/ssh/ssh_host_*
-    mkdir -p /mnt/ubuntu/root/.ssh
-    cd /mnt/ubuntu
-    tar -czf /tmp/ubuntu.tar.gz *
+    exit 1
 fi
+
+apt-get update
+apt-get install -y debootstrap
+
+mkdir -p /mnt/ubuntu
+
+packages="git-core,curl,ca-certificates,tmux,openssh-client,openssh-server,vim,redis-server"
+components="main,restricted,universe,multiverse"
+
+debootstrap --include "${packages}" --components="${components}" --arch amd64 xenial /mnt/ubuntu http://archive.ubuntu.com/ubuntu/
+
+curl -s https://raw.githubusercontent.com/Jumpscale/developer/master/mascot?$RANDOM > /mnt/ubuntu/root/.mascot.txt
+
+touch /mnt/ubuntu/root/.iscontainer
+chmod 1777 /mnt/ubuntu/tmp
+
+rm -rf /mnt/ubuntu/etc/ssh/ssh_host_*
+mkdir -p /mnt/ubuntu/root/.ssh
+
+cd /mnt/ubuntu
+tar -czf /tmp/ubuntu.tar.gz *
